c8f642b626bfbaf13a952a178ab1358622128bf1
Glitch (hello-express) <none>
2020-10-25 19:38:34 +0000

    👘🌟 Checkpoint ./server.js:10311809/11453 ./package.json:10311809/2609

--- server.js
+++ server.js
@@ -1,52 +1,111 @@
-const albumsData = [
-  {
-    albumId: "10",
-    artistName: "Beyoncé",
-    collectionName: "Lemonade",
-    artworkUrl100:
-      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
-    releaseDate: "2016-04-25T07:00:00Z",
-    primaryGenreName: "Pop",
-    url:
-      "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
-  },
-  {
-    albumId: "11",
-    artistName: "Beyoncé",
-    collectionName: "Dangerously In Love",
-    artworkUrl100:
-      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
-    releaseDate: "2003-06-24T07:00:00Z",
-    primaryGenreName: "Pop",
-    url:
-      "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
-  },
-];
 
-app.get("/albums", function (req, res) {
-  res.send(albumsData);
-});
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
 const app = express();
+const PORT = 3000;
 
-const quotes = require("./quotes.json");
+//app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-  response.send('/quotes/17 should return one quote, by id')
+let welcomeMessage = [
+  {
+  id: 0,
+  from: "Bart",
+  text: "Welcome to CYF chat system!",
+},
+ {
+  id: 1,
+  from: "jeff",
+  text: "Hello world",
+},
+{
+  id: 2,
+  from: "John Snow",
+  text: "Winter is coming!",
+},
+{
+  id: 3,
+  from: "Arnold",
+  text: "I'll be BACK",
+},
+{
+    id: 4,
+    from: "stallone",
+    text: "get up rocky",
+  },
+  {
+    id: 5,
+    from: "Alfy",
+    text: "have a nice day",
+  }
+
+]
+
+//This array is our "data store".
+//We will start with one message in the array.
+//Note: messages will be lost when Glitch restarts our server.
+
+//Last 10 messages ----change 10 to lesser number to test
+app.get('/messages/latest', (req, res) => {
+   res.json(welcomeMessage.slice(1).slice(-10));
+})
+//Display homepage
+app.get("/", function (request, response) {
+  //response.sendFile(__dirname + "/index.html");
+  response.send("Welcome to Sadat's chat server)
+});
+//Post a new message
+app.post('/messages', (req, res) => {
+    let myMessage = req.body;
+    if(Object.keys(myMessage).length === 0){
+      res.json({status: "400"});
+    }
+    myMessage?(welcomeMessage.push(myMessage),res.json(welcomeMessage)) : res.send({success: false});
+    console.log(welcomeMessage);
+  })
+
+//See all messages
+app.get('/messages', (req, res) => {
+  res.json(welcomeMessage);
+  console.log(welcomeMessage);
 });
 
-app.get("/quotes", function(request, response){
-  response.json(quotes);
-});
+//Getting a weird error ...works with "cyf" but not with other words / returns results from only the first element 
+app.get('/messages/search', (req, res) => {
+  let termQ = req.query.term;
+
+  welcomeMessage.filter((e, i) => {
+    e.text.toLowerCase().includes(termQ) ? res.json(e.text) : res.json({success: false});
+  })
+ 
+  
+})
+//Retrieve by ID
+app.get('/messages/:id', (req, res) => {
+  const {id} = req.params;
+  const filteredMessage =  welcomeMessage.find(e => e.id == id);
+  res.json(filteredMessage);
+  console.log(filteredMessage);
+
+})
+
+
+
+//Delete a message
+app.delete('/messages/:id', (req, res) => {
+  const {id} = req.params;
+  welcomeMessage = welcomeMessage.filter((e) => e.id != id);
+  id <= welcomeMessage.length ? (res.json(welcomeMessage),console.log(welcomeMessage)) : res.json({success: false}); 
+})
+
+
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
 
 app.listen(process.env.PORT);
 
-app.get('/quotes/:id', (req, res) => {
-  const {id} = req.params
-  const myQuotes = quotes.find(e => e.id == id);
-  myQuotes? res.json(myQuotes) : res.send('not found!');
-})
-app.get('/albums', (req, res) => {
-  
-  res.json(albumsData);
-})
